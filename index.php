317f6a24eba308b00459ab4a7e5b87d100c94217
Shxwndhsh <75093792+Shxwndhsh@users.noreply.github.com>
2023-02-24 02:13:30 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Smartphone Discussion Forum</title>
+    <link rel="StyleSheet" href="StyleSheet.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Forum</a>
+            <a href="#">About Us</a>
+            <a href="#">Contact Us</a>
+        </nav>
+    </header>
+    <main>
+        <section>
+            <h1>Welcome to the Smartphone Discussion Forum</h1>
+            <p>Join the conversation and discuss the latest smartphones, trends, and news with fellow tech enthusiasts!</p>
+        </section>
+        <section>
+            <h2>Recent Posts</h2>
+            <ul>
+                <li><a href="#">iPhone 13: Is It Worth the Hype?</a></li>
+                <li><a href="#">Samsung Galaxy S21: Our Review</a></li>
+                <li><a href="#">Google Pixel 6: What We Know So Far</a></li>
+            </ul>
+        </section>
+        <section>
+            <h2>Popular Topics</h2>
+            <ul>
+                <li><a href="#">Apple</a></li>
+                <li><a href="#">Samsung</a></li>
+                <li><a href="#">Google</a></li>
+                <li><a href="#">OnePlus</a></li>
+                <li><a href="#">Xiaomi</a></li>
+            </ul>
+        </section>
+        <section>
+            <h2>Join the Conversation</h2>
+            <form>
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name"><br>
+
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email"><br>
+
+                <label for="message">Message:</label><br>
+                <textarea id="message" name="message"></textarea><br>
+
+                <input type="submit" value="Submit">
+            </form>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2023 Smartphone Discussion Forum</p>
+    </footer>
+</body>
+</html>
